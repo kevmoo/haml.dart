6d761d441085dec622c45266a81e7b7cd1455e77
Kevin Moore <kevin@thinkpixellab.com>
2013-04-01 01:48:54 -0400

    this is crazy!!!

--- lib/src/haml/block_parser.dart
+++ lib/src/haml/block_parser.dart
@@ -7,45 +7,234 @@ class Block {
   Block(this.header, Iterable<Block> items) :
     this.children = new ReadOnlyCollection(items) {
     assert(header != null);
-    assert(!header.isEmpty);
     assert(children != null);
     assert(children.every((b) => b != null));
   }
 
-  static Sequence<Block> getBlocks(String source) {
+  static Iterable<Block> getBlocks(String source) {
     assert(source != null);
 
-    // whitespace: spaces + tabs
-    // ignore whitespace-only lines
-
+    return new _BlockIterable(source);
   }
 }
 
-/*
-class _BlockIterable extends Iterable<Block> {
+class _LineIterable extends Iterable<_Line> {
   final String source;
 
-  _BlockIterable(this.source);
+  _LineIterable(this.source);
+
+  Iterator<_Line> get iterator => new _LineIterator(source);
+}
+
+class _LineIterator extends Iterator<_Line> {
+  final StringLineReader _reader;
+
+  int _indentUnit;
+  int _indentRepeat;
+
+  _Line _current;
+
+  _LineIterator(String source) : this._reader = new StringLineReader(source);
+
+  _Line get current => _current;
+
+  _Line peek() {
+    final value = _reader.peekNextLine();
+    return _process(value);
+  }
+
+  bool moveNext() {
+    final line = _reader.readNextLine();
+    _current = _process(line);
+
+    // if _current has an empty value and we're at the end of the reader
+    // this this is a throw-away final, empty line. Just call it done.
+    if(_current != null && _current.value.isEmpty && _reader.eof) {
+      _current = null;
+    }
+    print(_current);
+    return _current != null;
+  }
+
+  _Line _process(String value) {
+    final line = _Line.parse(value, _indentUnit, _indentRepeat);
+    if(_indentUnit == null && line is _LinePlus) {
+      assert(_indentRepeat == null);
+      assert(line.level == 1);
+      _indentUnit = line.indentUnit;
+      _indentRepeat = line.indentRepeat;
+    }
+    return line;
+  }
+}
+
+class _LinePlus extends _Line {
+  final int indentUnit;
+  final int indentRepeat;
+
+  _LinePlus(String value, this.indentUnit, this.indentRepeat) :
+    super(1, value) {
+    assert(_Line.isWhite(indentUnit));
+    assert(indentRepeat > 0);
+  }
+}
+
+class _Line {
+  final int level;
+  final String value;
+
+  _Line(this.level, this.value) {
+    assert(level >= 0);
+    assert(value != null);
+    assert(!value.isEmpty);
+    assert(value.codeUnitAt(0) != _tab);
+    assert(value.codeUnitAt(0) != _space);
+  }
+
+  const _Line.empty() : this.level = null, this.value = '';
+
+  String toString() => '$level\t$value';
+
+  static _Line parse(String line, int indentUnit, int indentRepeat) {
+    if(line == null) return null;
+    if(line.isEmpty) return const _Line.empty();
+
+    if(indentUnit == null) {
+      if(isWhite(line.codeUnits.first)) {
+        // this could be bad. If the rest of the string isn't white, then throw!
+        if(line.codeUnits.every(isWhite)) {
+          return const _Line.empty();
+        }
+        assert(indentRepeat == null);
+
+        // entering a new world.
+        // We are going to try to infer what the indent type and repeat is
+        // it might be invalid. We'll see.
+        indentRepeat = 0;
+        for(final unit in line.codeUnits) {
+          if(indentUnit == null) {
+            // this is the first character. We proved earlier that it must
+            // be whitespace, right?
+            assert(isWhite(unit));
+            if(unit == _space) {
+              indentUnit = _space;
+            } else {
+              assert(unit == _tab);
+              indentUnit = _tab;
+            }
+          } else if(!isWhite(unit)) {
+            // we're out of whitespace. indentRepeat is valid
+            break;
+          } else if(unit != indentUnit) {
+            // we have mixed chars for indent. Bad!
+            throw 'mixed characters for indent is bad...';
+          }
+
+          indentRepeat++;
+        }
+
+        return new _LinePlus(line.substring(indentRepeat), indentUnit, indentRepeat);
+      }
+      return new _Line(0, line);
+    } else {
+      // so we have a level, which means
+      assert(isWhite(indentUnit));
+      assert(indentRepeat > 0);
+
+      final indent = new String
+          .fromCharCodes(line.codeUnits.takeWhile((u) => u == indentUnit));
+
+      assert(line.codeUnitAt(indent.length) != indentUnit);
+      if(isWhite(line.codeUnitAt(indent.length))) {
+        throw 'mixed whitespace indent...bad!';
+      }
+
+      final mod = indent.length % indentRepeat;
+      if(mod != 0) {
+        throw 'inconsistent indention, fool!';
+      }
+
+      final level = indent.length ~/ indentRepeat;
+
+      return new _Line(level, line.substring(indent.length));
+    }
+
+    throw 'should never get here...right?';
+  }
+
+  static bool isWhite(int unit) => unit == _tab || unit == _space;
+
+  static final int _tab = '\t'.codeUnits.single;
+  static final int _space = ' '.codeUnits.single;
+}
+
+class _BlockIterable extends Iterable<Block> {
+  final _LineIterable source;
+
+  _BlockIterable(String value) : source = new _LineIterable(value);
 
   Iterator<Block> get iterator => new _BlockIterator(source);
 }
 
-class _BlockIterator extends Iterator<Block> {
-  final StringLineReader reader;
+class _OnceIterable<E> extends Iterable<E> {
+  final Iterator<E> _value;
+  bool _requested = false;
 
-  _BlockIterator(String source) : this.reader = new StringLineReader(source) {
-    assert(source != null);
+  _OnceIterable(this._value);
+
+  Iterator<E> get iterator {
+    require(!_requested, 'Can only be iterated once!');
+    _requested = true;
+    return _value;
+  }
+
+}
+
+class _BlockIterator extends Iterator<Block> {
+  final _LineIterator reader;
+  final int level;
+  Block _current;
+  bool _done = false;
+
+  _BlockIterator(_LineIterable source) :
+    this.reader = source.iterator, this.level = 0;
+
+  _BlockIterator.child(this.reader, this.level) {
+    // child iterators should not be at level 0
+    assert(level > 0);
   }
 
   bool moveNext() {
-    if(reader.eof) {
-      return false;
+    if(!_done && reader.moveNext()) {
+      final currentLine = reader.current;
+      assert(currentLine.level == level);
+
+      final nextLine = reader.peek();
+
+      // if the next line is the same level, then we have a blank block
+      if(nextLine == null || nextLine.level == level) {
+        _current = new Block(currentLine.value, []);
+        return true;
+      } else if(nextLine.level < level) {
+        _current = new Block(currentLine.value, []);
+        _done = true;
+        return true;
+      } else if(nextLine.level == level + 1) {
+        // we are indenting, eh?
+        final childIterator = new _BlockIterator.child(reader, level + 1);
+        final childIterable = new _OnceIterable(childIterator);
+
+        _current = new Block(currentLine.value, childIterable);
+        return true;
+      } else if(nextLine.level > (level + 1)) {
+        throw 'next level is indented too much';
+      }
+
+      throw 'should never get here, right?';
     }
-
-
+    return false;
   }
 
+  Block get current => _current;
 
-  Block get current => null;
-
-}*/
+}
--- test/block_parser_test.dart
+++ test/block_parser_test.dart
@@ -2,21 +2,71 @@ library test.block_parser;
 
 import 'package:unittest/unittest.dart';
 import 'package:okoboji/haml.dart';
+import 'package:bot/bot.dart';
 
 void main() {
   group('block parser', () {
 
-    test('blank to empty', () {
-      final blocks = Block.getBlocks('');
-      expect(blocks, isEmpty);
+    test('runes and code units and spaces and tabs', () {
+      final spaceTab = ' \t';
+      expect(spaceTab, hasLength(2));
+      expect(spaceTab.codeUnits, hasLength(2));
+      expect(spaceTab.runes, hasLength(2));
+
+      expect(3 % 2, equals(1));
+      expect(3 % 1, equals(0));
+      expect(3 % 3, equals(0));
+      expect(3 % 4, equals(3));
+      expect(3 % 5, equals(3));
+      expect(3 % 6, equals(3));
     });
 
     test('blank to empty', () {
+      final blocks = Block.getBlocks('').toList();
+      expect(blocks, isEmpty);
+    });
+
+    test('just hello', () {
       final blocks = Block.getBlocks('hello').toList();
       expect(blocks, hasLength(1));
       final block = blocks.single;
       expect(block.header, equals('hello'));
       expect(block.children, isEmpty);
+    });
+
+    test('two lines', () {
+      final source = 'hello\ngoodbye\nlater';
+
+      final blocks = Block.getBlocks(source).toList();
+      expect(blocks, hasLength(3));
+
+      var block = blocks[0];
+      expect(block.header, equals('hello'));
+      expect(block.children, isEmpty);
+
+      block = blocks[1];
+      expect(block.header, equals('goodbye'));
+      expect(block.children, isEmpty);
+
+      block = blocks[2];
+      expect(block.header, equals('later'));
+      expect(block.children, isEmpty);
+    });
+
+    test('simple nesting', () {
+      final source = 'hello\n  hi\nlater\n  goodbye';
+
+      final blocks = Block.getBlocks(source).toList();
+      expect(blocks, hasLength(2));
+
+      expect(blocks[0].header, equals('hello'));
+      expect(blocks[0].children, hasLength(1));
+      expect(blocks[0].children[0].header, equals('hi'));
+      expect(blocks[0].children[0].children, isEmpty);
+      expect(blocks[1].header, equals('later'));
+      expect(blocks[1].children, hasLength(1));
+      expect(blocks[1].children[0].header, equals('goodbye'));
+      expect(blocks[1].children[0].children, isEmpty);
     });
 
   });
@@ -30,4 +80,9 @@ const _invalid = const {
 hello
   two spaces
 \ttab''',
+  'inconsistent indent': '''
+hello
+  two spaces - ok
+   three spaces - bad!
+'''
 };
