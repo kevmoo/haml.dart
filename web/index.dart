f311a309f8cf3f6e1d759d91e8ae420f66fdb1d8
Kevin Moore <kevin@thinkpixellab.com>
2013-04-29 10:20:39 -0400

    better display of errors n such

--- web/index.dart
+++ web/index.dart
@@ -6,22 +6,38 @@ import 'package:js/js.dart' as js;
 import 'package:okoboji/core.dart';
 import 'package:okoboji/haml.dart';
 
+html.DivElement _cmOutput;
 
 void main() {
   html.ButtonElement button = html.query('#convert');
 
   button.onClick.listen(_convertClick);
+
+  _cmOutput = html.query('.CodeMirror.outputCM');
 }
 
 void _convertClick(args) {
 
   var sourceVal = _getSourceValue();
 
-  _setOutputValue('working...\n\n' + sourceVal);
+  _setOutputValue('working...\n\n' + sourceVal, {'mode': 'text'});
 
   runAsync(() {
-    var parsedOutput = _test(sourceVal);
-    _setOutputValue(parsedOutput);
+    String content = '';
+    String mode = 'htmlmixed';
+    bool lineNumbers = true;
+    try {
+      content = _test(sourceVal);
+      _cmOutput.classes.remove('error');
+    } catch (e, stack) {
+      _cmOutput.classes.add('error');
+      mode = 'text';
+      lineNumbers = false;
+
+      content = e.toString() + '\n' + stack.toString();
+    }
+
+    _setOutputValue(content, {'lineNumbers': lineNumbers, 'mode': mode});
   });
 }
 
@@ -29,8 +45,12 @@ String _getSourceValue() {
   return js.context['window']['_sourceCodeMirror']['getValue']();
 }
 
-void _setOutputValue(String value) {
+void _setOutputValue(String value, Map options) {
   js.context['window']['_outputCodeMirror']['setValue'](value);
+
+  options.forEach((k, v) {
+    js.context['window']['_outputCodeMirror']['setOption'](k, v);
+  });
 }
 
 String _test(String input) {
