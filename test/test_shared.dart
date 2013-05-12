a0e1d1f2a7d22f36944be64f104d6c82e6786dc0
Kevin Moore <kevin@thinkpixellab.com>
2013-05-11 20:43:49 -0400

    moved haml standard tests locally
    starting to remove ruby-specific escaping. Felt like the right thing to do...

--- test/test_shared.dart
+++ test/test_shared.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'dart:json' as json;
 import 'package:pathos/path.dart' as pathos;
 
-const _jsonTestPath = 'resource/haml-spec/tests.json';
+const _jsonTestPath = 'test/tests.json';
 
 Map<String, Map> getHamlSpecTests() {
   final jsonFile = new File(pathos.normalize(_jsonTestPath));
